ea4adea6c511398266fb4305922013a50c9ed78f
arturdolzan <arturdolzan@gmail.com>
2024-06-19 20:06:13 -0300

    Simplified manage page UI

--- src/pages/manage/page.tsx
+++ src/pages/manage/page.tsx
@@ -1,24 +1,53 @@
 import { StringViewer } from 'components/stringViewer'
-import { ReactNode, useState } from 'react'
+import { useState } from 'react'
 import { SourceOfPrivateKeyType } from 'types/sourceOfPrivateKeyType'
 import { RadioBox } from './_components/radioBox'
 import { GeneratePkIcon } from 'icons/generatePkIcon'
 import { ImportPkIcon } from 'icons/importPkIcon'
+import { useBitcoinTestnetKey } from './_hooks/useBitcoinTestnetKey'
 
-const PrivateKey = ({ source }: { source: SourceOfPrivateKeyType }) =>
+interface PrivateKeyProps {
+  source: SourceOfPrivateKeyType
+  handleChange?: (text: string) => void
+  privateKey: string
+}
+
+const PrivateKey = ({ source, handleChange, privateKey }: PrivateKeyProps) =>
   source === 'generate' ? (
-    <StringViewer text="privatekey xxxxxxxxxxx" title="Private Key" />
+    <StringViewer
+      enableCopyToClipboard={true}
+      text={privateKey}
+      title="Private Key"
+    />
   ) : (
     <StringViewer
+      enableEditing={true}
+      onTextChange={handleChange}
       placeholder="Paste your Private Key here.."
-      text=""
+      text={privateKey}
       title="Private Key"
     />
   )
 
 export const ManagePage = function () {
+  const { generateKey } = useBitcoinTestnetKey()
+  const [bitcoinKey, setBitcoinKey] = useState(generateKey)
   const [sourceOfPrivateKey, setSourceOfPrivateKey] =
     useState<SourceOfPrivateKeyType>('generate')
+
+  const handleSourceChange = (source: SourceOfPrivateKeyType) => {
+    setSourceOfPrivateKey(source)
+    if (source === 'generate') {
+      const key = generateKey()
+      setBitcoinKey(key)
+    } else {
+      setBitcoinKey({ address: '', privateKey: '' })
+    }
+  }
+
+  const handlePrivateKeyChange = (text: string) => {
+    setBitcoinKey({ address: '', privateKey: text })
+  }
 
   return (
     <div className="grid w-full grid-cols-3-column-layout">
@@ -38,20 +67,24 @@ export const ManagePage = function () {
                 icon={<GeneratePkIcon />}
                 id="generate"
                 label="Generate a Private Key"
-                onChange={() => setSourceOfPrivateKey('generate')}
+                onChange={() => handleSourceChange('generate')}
               />
               <RadioBox
                 checked={sourceOfPrivateKey === 'import'}
                 icon={<ImportPkIcon />}
                 id="import"
                 label="Input your own Private Key"
-                onChange={() => setSourceOfPrivateKey('import')}
+                onChange={() => handleSourceChange('import')}
               />
             </div>
             <div className="h-px w-full border border-solid border-zinc-300/55" />
-            <PrivateKey source={sourceOfPrivateKey} />
-            <div className="mt-5">
-              <button className="h-14 w-full cursor-pointer rounded-xl bg-orange-950 text-base text-white hover:bg-opacity-80">
+            <PrivateKey
+              handleChange={handlePrivateKeyChange}
+              source={sourceOfPrivateKey}
+              privateKey={bitcoinKey.privateKey}
+            />
+            <div className="mt-12">
+              <button className="h-14 w-full cursor-pointer rounded-xl bg-orange-950 text-lg text-white hover:bg-opacity-80">
                 Continue
               </button>
             </div>
