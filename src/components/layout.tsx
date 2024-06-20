694564923c08b167e36acac7871f100f8921b6af
arturdolzan <arturdolzan@gmail.com>
2024-06-20 18:39:47 -0300

    Implement designed for desktop comp and layout fixes

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -3,6 +3,7 @@ import { Outlet, useLocation, useNavigate } from 'react-router-dom'
 import { Footer } from './footer'
 import { Header } from './header'
 import { useEffect } from 'react'
+import { DesignForDesktop } from './designForDesktop'
 
 export const Layout = function () {
   const { pathname } = useLocation()
@@ -18,10 +19,19 @@ export const Layout = function () {
   )
 
   return (
-    <div className="flex h-screen w-full flex-col justify-between px-16 py-8 2xl:mx-auto 2xl:max-w-screen-2xl">
-      <Header />
-      <Outlet />
-      <Footer />
+    <div className="flex min-h-screen flex-col px-12 py-8">
+      <div className="flex-shrink-0 ">
+        <Header />
+      </div>
+      <div className="py-16 md:hidden">
+        <DesignForDesktop />
+      </div>
+      <div className="hidden flex-grow items-center justify-center md:flex">
+        <Outlet />
+      </div>
+      <div className="hidden flex-shrink-0 md:flex">
+        <Footer />
+      </div>
     </div>
   )
 }
