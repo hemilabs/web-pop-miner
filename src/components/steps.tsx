acadc56c54837b86c86650d6375842162b18cd25
Artur Dolzan Neto <36503078+ArturDolzan@users.noreply.github.com>
2024-06-24 11:04:23 -0300

    Implement stepper component and layout improvements (#9)

--- src/components/steps.tsx
+++ src/components/steps.tsx
@@ -1,12 +1,19 @@
 import { useLocation } from 'react-router-dom'
+import { Stepper } from './stepper'
 
 export const Steps = function () {
   const { pathname } = useLocation()
-  if (pathname.endsWith('manage')) {
-    return <span className="text-center">Manage private key</span>
+  const steps = ['Manage Private Key', 'Fund Address']
+
+  const routeToStep: { [key: string]: number } = {
+    manage: 0,
+    fund: 1,
   }
-  if (pathname.endsWith('fund')) {
-    return <span className="text-center">Fund address</span>
-  }
-  return null
+
+  const foundKey = Object.keys(routeToStep).find(key => pathname.endsWith(key))
+  const currentStep = foundKey !== undefined ? routeToStep[foundKey] : -1
+
+  if (currentStep === -1) return null
+
+  return <Stepper currentStep={currentStep} steps={steps} />
 }
