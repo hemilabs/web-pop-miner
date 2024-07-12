6345086b6d7222e32f4b2b166da971703d4c32f0
arturdolzan <arturdolzan@gmail.com>
2024-07-12 17:13:30 -0300

    Fix not found mempool error

--- src/pages/explorer/_hooks/useBtcTransactions.ts
+++ src/pages/explorer/_hooks/useBtcTransactions.ts
@@ -1,4 +1,5 @@
 import { useQueries, UseQueryOptions } from '@tanstack/react-query'
+import camelcaseKeys from 'camelcase-keys'
 import fetch from 'fetch-plus-plus'
 import { Satoshi } from 'types/Satoshi'
 
@@ -13,10 +14,10 @@ type Vout = {
 }
 
 type Status = {
+  blockHeight?: number
+  blockTime?: number
+  blockHash?: number
   confirmed: boolean
-  block_height: number
-  block_hash: string
-  block_time: number
 }
 
 export type Transaction = {
@@ -33,24 +34,35 @@ export type Transaction = {
   cost: number
 }
 
-const fetchTransaction = async (hash: string): Promise<Transaction> => {
-  const response = await fetch(
-    `${import.meta.env.VITE_PUBLIC_MEMPOOL_API_URL}/api/tx/${hash}`,
-  )
-  const data = (await response) as Omit<Transaction, 'cost'>
+const toCamelCase = <T extends Record<string, any> | readonly any[]>(obj: T) =>
+  camelcaseKeys(obj, { deep: true })
 
-  // Calculate total output value from original vout array
-  const totalOutSatoshi = data.vout.reduce((acc, vout) => acc + vout.value, 0)
-  // Calculate total input value from vin array
-  const totalInSatoshi = data.vin.reduce(
-    (acc, vin) => acc + vin.prevout.value,
-    0,
-  )
-
-  const cost = ((totalInSatoshi - totalOutSatoshi) / 100000000).toFixed(8)
-
-  return { ...data, cost: parseFloat(cost) }
-}
+const fetchTransaction = (hash: string): Promise<Transaction | null> =>
+  fetch(`${import.meta.env.VITE_PUBLIC_MEMPOOL_API_URL}/api/tx/${hash}`)
+    .catch(function (err) {
+      if (err?.message.includes('not found')) {
+        // It seems it takes a couple of seconds for the Tx for being picked up
+        // Once it appears in the mempool, it will return the full object
+        return null
+      }
+      throw err
+    })
+    .then(toCamelCase)
+    .then((data: Transaction) => {
+      if (!data) return null
+      // Calculate total output value from original vout array
+      const totalOutSatoshi = data.vout.reduce(
+        (acc, vout) => acc + vout.value,
+        0,
+      )
+      // Calculate total input value from vin array
+      const totalInSatoshi = data.vin.reduce(
+        (acc, vin) => acc + vin.prevout.value,
+        0,
+      )
+      const cost = ((totalInSatoshi - totalOutSatoshi) / 100000000).toFixed(8)
+      return { ...data, cost: parseFloat(cost) }
+    })
 
 export const useGetBtcBlockByTransaction = function (
   active: boolean,
@@ -81,12 +93,10 @@ export const useGetBtcBlockByTransaction = function (
   const transactions = queries
     .map(query => query.data)
     .filter(data => data) as Transaction[]
-  const sortedTransactions = transactions.sort((a, b) =>
-    a.status.confirmed === b.status.confirmed
-      ? b.status.block_time - a.status.block_time
-      : a.status.confirmed
-        ? 1
-        : -1,
+  const sortedTransactions = transactions.sort(
+    (a, b) =>
+      (b.status.blockTime ?? 0) - (a.status.blockTime ?? 0) ||
+      (a.status.confirmed ? 1 : -1),
   )
 
   return { transactions: sortedTransactions, isLoading, error }
