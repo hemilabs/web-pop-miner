e6c63f46763f5ca4ed87397e66d85d1147b70d74
arturdolzan <arturdolzan@gmail.com>
2024-07-16 12:36:56 -0300

    Implement PoP miner intro page

---
+++ src/pages/intro/_components/introSteps.tsx
@@ -0,0 +1,21 @@
+interface Props {
+  position: number
+  textBold: string
+  text: string
+}
+
+export const IntroSteps = ({ position, textBold, text }: Props) => (
+  <div className="flex items-start space-x-4">
+    <div className="flex items-center rounded-full border border-orange-200">
+      <div className="flex h-9 w-9 items-center justify-center rounded-full bg-orange-50">
+        <span className="p-4 text-xl font-medium text-orange-500">
+          {position}
+        </span>
+      </div>
+    </div>
+    <p className="text-left text-sm font-medium text-neutral-500">
+      <span className="mr-1 text-neutral-950">{textBold}</span>
+      {text}
+    </p>
+  </div>
+)
